8934632282cd8a31d8dc530fdaf2ebdf7d218c7b
kjhav <kjhav@Think-Pad-17.dhcp.wustl.edu>
2023-10-04 13:55:49 -0500

    Commit 1

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,59 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		StdDraw.setPenColor(r, g, b);
+		boolean filled = in.nextBoolean();
+		if (shape.equals("rectangle"))
+		{
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if (filled == true)
+			{
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else
+			{
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		if (shape.equals("ellipse"))
+		{
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+			if (filled == true)
+			{
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			else
+			{
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+		}
+		else
+		{
+			double[] x = new double[3];
+			double[] y = new double[3];
+			for (int i = 0; i < 3; i++)
+			{
+				x[i] = in.nextDouble();
+				y[i] = in.nextDouble();
+			}
+			if (filled == true)
+			{
+				StdDraw.filledPolygon(x, y);
+			}
+			else
+			{
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
